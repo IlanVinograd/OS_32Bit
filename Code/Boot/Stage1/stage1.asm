47b9da264e5d2b111f6b4862e3b258a326558e32
IlanVinograd <ilanv1111@gmail.com>
2024-09-11 22:54:45 +0300

    Updated code (but currently has a bug with stage 1) and Add some code to stage 2 to test the read disk.

--- Code/Boot/Stage1/stage1.asm
+++ Code/Boot/Stage1/stage1.asm
@@ -36,11 +36,12 @@ main:                   ; Main routine of the bootloader begins here.
     ; Load Stage 2 bootloader from disk
     ; -------------------------
     mov ah, 02h         ; BIOS Interrupt 13h, Function 02h: Read sectors from the disk.
-    mov al, 62h         ; Read 63 sectors (this should correspond to the size of Stage 2). Ensure this number does not exceed the size of Stage 2 to avoid reading unnecessary code.
+    mov al, 01h         ; Read 63 sectors (this should correspond to the size of Stage 2). Ensure this number does not exceed the size of Stage 2 to avoid reading unnecessary code.
 
     mov ch, 00h         ; Set Cylinder number to 1 (since Stage 1 is at Cylinder 0, Stage 2 starts at Cylinder 1).
     mov cl, 02h         ; Set Sector number to 1 (the first sector on the cylinder to read from).
     mov dh, 00h         ; Set Head number to 0 (assuming we are using Head 0 for now).
+    mov dl, 0x80
 
     mov es, ax          ; Set ES to the address where Stage 2 should be loaded (0x7C0).
     mov bx, 0x8000      ; Set BX to 0x8000, the memory address where Stage 2 will be loaded.
--- Code/Boot/Stage2/stage2.asm
+++ Code/Boot/Stage2/stage2.asm
@@ -1,1 +1,26 @@
+[BITS 16]
+[ORG 0x8000]
 
+start:
+    mov si, message
+    call print_string
+
+    cli
+    hlt
+
+print_string:
+    mov ah, 0x0E
+.next_char:
+    lodsb
+    cmp al, 0
+    je done
+    int 0x10
+    jmp .next_char
+
+done:
+    ret
+
+message db "Hello from Stage 2! :)", 0
+
+TIMES 510-($-$$) db 0
+DW 0xAA55
