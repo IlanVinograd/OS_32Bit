18c7edfffda40fcbe5a9331efa81a2b68c83e258
IlanVinograd <ilanv1111@gmail.com>
2024-10-03 15:05:44 +0300

    Add new libraries

---
+++ Code/Kernel/Includes/stdlib.h
@@ -0,0 +1,7 @@
+#ifndef _STDLIB_H_
+#define _STDLIB_H_
+
+void itoa(int num, char* str, int base);
+int atoi(const char* str); // <-- Function needs to be implemented !!!!
+
+#endif /* _STDLIB_H_ */
