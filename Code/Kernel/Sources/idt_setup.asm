ebd4ff1b705abc02b27bef41a69940c6ec0b43d1
Michael Petch <mpetch@gmail.com>
2024-10-10 15:25:33 -0600

    Clean up itnerrupt handers. CLI not needed at top of handler. Entries in the IDT that are interrupt gates cause the CPU to disable interrupts automatially before calling a handler.
    CLD is needed per the 32-bit System V ABI to ensure string
    instruction like MOVS, STOS, CMPS move in the dorward direction.
    
    Exceptionss aren't external interrupts, therefore you should
    not be sending EOIs to PICs. Only send EOIs if an external
    interrupt (IRQ0 to IRQ15) are received.

--- Code/Kernel/Sources/idt_setup.asm
+++ Code/Kernel/Sources/idt_setup.asm
@@ -19,54 +19,53 @@ load_idt:
     lidt [idt_p]       ; Load IDT pointer into the IDTR register
     ret
 
+; No need for CLI as interrupt gates have interrupts turned off
+; by the CPU before they start executing a handler.
+
 ; ISR for Divide by Zero Exception (ISR0)
 isr0:
-    cli                ; Disable interrupts
     pusha              ; Save all registers
 
+    cld                ; DF = 0 required by 32-bit System V ABI
     call isr0_handler  ; Call the C function to print the exception message
 
     popa               ; Restore registers
-    out 0x20, al       ; Send End of Interrupt (EOI) to the PIC
 
     hlt                ; Halt the CPU to stop further execution
     jmp $              ; Infinite loop to halt system
 
 ; ISR for Invalid Opcode Exception (ISR6)
 isr6:
-    cli                ; Disable interrupts
     pusha              ; Save all registers
 
+    cld                ; DF = 0 required by 32-bit System V ABI
     call isr6_handler  ; Call the C function to print the Invalid Opcode message
 
     popa               ; Restore registers
-    out 0x20, al       ; Send End of Interrupt (EOI) to the PIC
 
     hlt                ; Halt the CPU to stop further execution
     jmp $              ; Infinite loop to halt system
 
 ; ISR for General Protection Fault Exception (ISR13)
 isr13:
-    cli                ; Disable interrupts
     pusha              ; Save all registers
 
+    cld                ; DF = 0 required by 32-bit System V ABI
     call isr13_handler ; Call the C function to print the General Protection Fault message
 
     popa               ; Restore registers
-    out 0x20, al       ; Send End of Interrupt (EOI) to the PIC
 
     hlt                ; Halt the CPU to stop further execution
     jmp $              ; Infinite loop to halt system
 
 ; ISR for Page Fault Exception (ISR14)
 isr14:
-    cli                ; Disable interrupts
     pusha              ; Save all registers
 
+    cld                ; DF = 0 required by 32-bit System V ABI
     call isr14_handler ; Call the C function to print the Page Fault message
 
     popa               ; Restore registers
-    out 0x20, al       ; Send End of Interrupt (EOI) to the PIC
 
     hlt                ; Halt the CPU to stop further execution
     jmp $              ; Infinite loop to halt system
