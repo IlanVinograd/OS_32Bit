b59c69e1cce04e3f6534bf108473cbbf772fc9dd
IlanVinograd <ilanv1111@gmail.com>
2024-10-18 00:26:22 +0300

    fix

--- Code/Kernel/Sources/detect_memory.c
+++ Code/Kernel/Sources/detect_memory.c
@@ -4,9 +4,13 @@ void init_memory_map() {
     memory_map_entry_t* mem_map = (memory_map_entry_t*)MEMORY_MAP_VADDR;
     
     uint8_t count = *((uint8_t*)COUNT_ENTRIES_VADDR);
-    printf("Memory map entry count: %d\n", COLOR_BLACK_ON_WHITE, count);
+    printf("Memory map entry count: %d\n\n", COLOR_BLACK_ON_WHITE, count);
 
     for (int i = 0; i < count; i++) {
-            printf("Usable memory: Base: 0x%llx, Length: 0x%llx, type: %d\n", COLOR_BLACK_ON_WHITE, mem_map[i].base_addr, mem_map[i].length, mem_map[i].type);
+        if(mem_map[i].type == 1){
+            printf("Usable memory: Base: 0x%llx, Length: 0x%llx, type: %d\n", GREEN_ON_BLACK_SUCCESS, mem_map[i].base_addr, mem_map[i].length, mem_map[i].type);
+        } else {
+            printf("Usable memory: Base: 0x%llx, Length: 0x%llx, type: %d\n", RED_ON_BLACK_WARNING, mem_map[i].base_addr, mem_map[i].length, mem_map[i].type);
+        }
     }
 }
