6c1c1005cf91c7646e080518d1a53bcc382ed938
Michael Petch <mpetch@gmail.com>
2024-11-16 15:45:23 -0700

    pit_handler fixes

--- Code/Kernel/Sources/PIT.c
+++ Code/Kernel/Sources/PIT.c
@@ -10,15 +10,15 @@ void pit_init(uint32_t frequency) {
     outPort(PIT_CHANNEL0, (uint8_t)((divisor >> 8) & 0xFF));
 }
 
-void pit_handler(void) { // each 10ms will be interrupt. 
-    __asm__("cli"); // CLI to avoid recursive.    
+void pit_handler(void) { // each 10ms will be interrupt.
+    // IRQ handlers ar set as interrupt gates in the IDT so interrupts
+    // will always be off a this point as a result. No CLI necessary.
     tick_count++;
 
     setCursorPosition(24, 0);
     printf("Bitmap state: Pages ( %d / %d ) (TASKS: %d) - PIT Ticks - %d \n", COLOR_BLACK_ON_WHITE, pagesAllocated, NUM_PAGES, nowTasks, tick_count);
-    
     pic_send_eoi(0);
-    schedule(); // Enable Interrupt happening in switch_to_task();
+    schedule(); // Will enable interrupts
 }
 
 uint32_t get_tick_count(void) {
