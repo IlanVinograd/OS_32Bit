bc690b8978b2994eb1d928ba66ac7d5135087a2b
IlanVinograd <ilanv1111@gmail.com>
2025-01-17 23:05:09 +0200

    .

--- Code/Kernel/Sources/keyboard.c
+++ Code/Kernel/Sources/keyboard.c
@@ -205,7 +205,6 @@ void handle_enter() {
     setCursorPosition(row, 0);
 
     if (strcmp((const uint8_t*)parsedCommand.command, (const uint8_t*)"test") == 0) {
-    if (strcmp((const uint8_t*)parsedCommand.command, (const uint8_t*)"test") == 0) {
         if (parsedCommand.arg_count > 0 && strcmp(parsedCommand.arguments[0], "--all") == 0) {
             // Allocate all memory
             test_full_allocation();
